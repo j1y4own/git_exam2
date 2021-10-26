3c0cdd5cdca4530ec631658368a400c03489e546
전지윤 <missji1004@gmail.com>
2021-10-26 09:39:35 +0900

    201814113-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,9 +3,9 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("201814113");
 		System.out.println("전지윤");
 	}
 
 }
-
